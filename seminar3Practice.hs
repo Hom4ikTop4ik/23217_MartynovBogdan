74b556196d884ee9c38297fdc2e1b21e02758816
Hom4ikTop4ik <85843337+Hom4ikTop4ik@users.noreply.github.com>
2023-09-29 11:50:25 +0700

    seminar3Practice.hs
    14 строчка:
    было "< -1"
    стало "< 0"

--- seminar3Practice.hs
+++ seminar3Practice.hs
@@ -11,6 +11,6 @@ res2 f p = res1 f (\x -> e**p) p
 
 -- Zadacha 3
 res3 :: (Double -> Double) -> Double -> Double -> Double
-res3 f p n | n < -1 = error "pls, use n >= 0"
+res3 f p n | n < 0 = error "pls, use n >= 0"
            | n == 0 = p
            | otherwise = res3 f (f p) (n-1)
