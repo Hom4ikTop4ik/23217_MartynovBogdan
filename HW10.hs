e1c452773294455044dc7034bc75afe7b7d71482
Hom4ikTop4ik <85843337+Hom4ikTop4ik@users.noreply.github.com>
2023-12-25 01:57:34 +0700

    Update HW10.hs
    — fromList не работает при пустом списке
    — теперь работает
    
    — сравнивать конструкторы данных с помощью == -- это плохой стиль.
    — согласен, исправил
    
    — repeat' -- есть же удобная функция replicate)
    — исправлено
    
    — isSearchTree не рекурсивная. Проверяет свойство только для корня, а глубже не идет.
    — Теперь рекурсивная ("простите за строку длиной в 3 экрана")

--- HW10.hs
+++ HW10.hs
@@ -13,6 +13,7 @@ data BinTree a =
 
 -- для более удобного вывода
 instance Show a => Show (BinTree a) where
+    show :: Show a => BinTree a -> String
     show = show0 0 where
         show0 _ Nil = "Nil"
         show0 lvl Node{left=l, right=r, value=v, count=cnt} =
@@ -20,10 +21,7 @@ instance Show a => Show (BinTree a) where
             replicate lvl '\t' ++ "l=" ++ show0 (lvl+1) l ++ "\n" ++
             replicate lvl '\t' ++ "r=" ++ show0 (lvl+1) r ++ "\n"
             -- replicate lvl '\t' — печатает lvl раз символ '\t'
-instance Eq a => Eq (BinTree a) where
-    (==) tree1 tree2 = case (tree1, tree2) of
-        (Nil, Nil) -> True
-        _ -> False
+
 
 testTree :: BinTree Int
 testTree = Node {
@@ -51,18 +49,23 @@ testTree = Node {
     value = 8, count = 1
 }
 
+empty_tree = Nil
+
+one_fifteen_tree = fromList [8,4,12,2,6,10,14,1,3,5,7,9,11,13,15]
+
 ---------
 -- task 1
 
 insert :: Ord a => BinTree a -> a -> BinTree a
-insert tree new_value | tree == Nil    = Node{left=Nil, right=Nil, value=new_value, count=1}
-                      | new_value == v = Node{left=l, right=r, value=v, count=cnt+1}
-                      | new_value < v  = Node{left=(insert l new_value), right=r, value=v, count=cnt}
-                      | new_value > v  = Node{left=l, right=(insert r new_value), value=v, count=cnt} 
-                      where Node{left=l, right=r, value=v, count=cnt} = tree
+insert Nil new_value = Node{left=Nil, right=Nil, value=new_value, count=1}
+insert Node{left=l, right=r, value=v, count=cnt} new_value 
+        | new_value == v = Node{left=l, right=r, value=v, count=cnt+1}
+        | new_value < v  = Node{left=(insert l new_value), right=r, value=v, count=cnt}
+        | new_value > v  = Node{left=l, right=(insert r new_value), value=v, count=cnt}
 
 -- for unoptimized tree (very easy)
 fromList :: Ord a => [a] -> BinTree a
+fromList [] = Nil
 fromList list = foldl (\acc x -> insert acc x) (insert Nil (head list)) (tail list) 
 
 -- for optimized tree (harder)
@@ -85,56 +88,57 @@ fromList list = foldl (\acc x -> insert acc x) (insert Nil (head list)) (tail li
 -- task 2
 
 findMin :: Ord a => BinTree a -> Maybe a
-findMin tree    | tree == Nil = Nothing
-                | l == Nil    = Just v
-                | otherwise   = findMin l
-                where Node{left=l, right=r, value=v, count=cnt} = tree
+findMin Nil                                         = Nothing
+findMin Node{left=Nil, right=r, value=v, count=cnt} = Just v
+findMin Node{left=l, right=r, value=v, count=cnt}   = findMin r
 
 findMax :: Ord a => BinTree a -> Maybe a
-findMax tree    | tree == Nil = Nothing
-                | r == Nil    = Just v
-                | otherwise   = findMax r
-                where Node{left=l, right=r, value=v, count=cnt} = tree
+findMax Nil                                         = Nothing
+findMax Node{left=l, right=Nil, value=v, count=cnt} = Just v
+findMax Node{left=l, right=r, value=v, count=cnt}   = findMax r
 
 ---------
 -- task 3
 treeSort :: Ord a => BinTree a -> [a]
 treeSort Nil = []
-treeSort tree = (treeSort l) ++ (repeat' tree) ++ (treeSort r) 
+treeSort tree = (treeSort l) ++ (replicate cnt v) ++ (treeSort r) 
                 where Node{left=l, right=r, value=v, count=cnt} = tree
 
-repeat' ::  Ord a => BinTree a -> [a]
-repeat' tree  = let Node{left=l, right=r, value=v, count=cnt} = tree in 
-                fst(foldl (\(acc, val) x -> (val:acc, val)) ([], v) [1..cnt]) 
+-- my analog of replicate
+-- repeat' ::  Ord a => BinTree a -> [a]
+-- repeat' tree  = let Node{left=l, right=r, value=v, count=cnt} = tree in 
+--                 fst(foldl (\(acc, val) x -> (val:acc, val)) ([], v) [1..cnt]) 
 
 ---------
 -- task4
 findAny :: Ord a => (a -> Bool) -> BinTree a -> Maybe a
-findAny pred tree   | tree == Nil = Nothing
-                    | pred v = Just v
+findAny _ Nil = Nothing
+findAny pred tree   | pred v = Just v
                     | otherwise = if (x == []) then Nothing else Just (head x)
-                    where Node{left=l, right=r, value=v, count=cnt} = tree; x = (catMaybes ([findAny pred l]++[findAny pred r]))
+                    where 
+                        Node{left=l, right=r, value=v, count=cnt} = tree
+                        x = (catMaybes ([findAny pred l]++[findAny pred r]))
 
 ---------
 -- task*
 
 -- first example for task*:
 --         5
---      7     5
+--      4     6
 --     3 1   9 4
 notSearchTree :: BinTree Int
 notSearchTree = Node {
     left = Node {
         left = Node{left=Nil, right=Nil, value=3, count=1}, 
         right = Node{left=Nil, right=Nil, value=1, count=1}, 
-        value = 7, count = 1
+        value = 4, count = 1
         },
     right = Node {
         left = Node {
             left = Nil, right = Nil, value = 9, count = 1},
         right = Node {
             left = Nil, right = Nil, value = 4, count = 1},
-        value = 5, count = 1
+        value = 6, count = 1
     },
     value = 5, count = 1
 }
@@ -160,16 +164,13 @@ searchTree = Node {
     value = 5, count = 1
 }
 
-
-
+-- Денис Сергеевич, простите, конечно, за строки длиной в 3 экрана, но по-другому не знаю как (нет времени на придумывание) 
 isSearchTree :: Ord a => BinTree a -> Bool
-isSearchTree tree = helper_isSearchTree tree True
-
-helper_isSearchTree tree acc = if (pred1 && pred2) then acc else False 
-                            where pred1 = if(l /= Nil) then vl < v else True; 
-                                  pred2 = if(r /= Nil) then v < vr else True; 
-                                  Node{left=l, right=r, value=v, count=cnt} = tree; 
-                                  Node{left=ll, right=rl, value=vl, count=cntl} = l; 
-                                  Node{left=lr, right=rr, value=vr, count=cntr} = r
-
----------   
+isSearchTree Nil = True
+isSearchTree Node{left=Nil, right=Nil, value=right_v, count=right_cnt} = True 
+isSearchTree Node{left=l@Node{left=left_l, right=left_r, value=left_v, count=left_cnt}, right=Nil, value=v, count=_} = 
+    (left_v < v) && isSearchTree l
+isSearchTree Node{left=Nil, right=r@Node{left=right_l, right=right_r, value=right_v, count=right_cnt}, value=v, count=_} = 
+    (v < right_v) && isSearchTree r
+isSearchTree Node{left=l@Node{left=left_l, right=left_r, value=left_v, count=left_cnt}, right=r@Node{left=right_l, right=right_r, value=right_v, count=right_cnt}, value=v, count=_} = 
+    (left_v < v) && (v < right_v) && isSearchTree l && isSearchTree r
