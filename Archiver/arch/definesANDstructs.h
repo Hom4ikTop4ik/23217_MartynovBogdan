2d76d7b7deecffabbc54382aaebf4a5f291a05f9
Hom4ikTop4ik <85843337+Hom4ikTop4ik@users.noreply.github.com>
2024-05-31 23:08:28 +0700

    Add files via upload

---
+++ Archiver/arch/definesANDstructs.h
@@ -0,0 +1,37 @@
+#ifndef definesANDstructs123
+#define definesANDstructs123
+
+	#define compressTree 1
+	
+	typedef unsigned char word;
+	#define bitInByte 8 // 1 word = 8 bit
+	#define differentBytes (1ll << (  bitInByte * sizeof(word)  ))
+	#define bufferSize 1024
+	#define fileNameBufferSize 1024
+
+    typedef struct STree {
+        int value;
+        struct STree* left;
+        struct STree* right;
+        int size;
+    } TTree;
+
+    typedef struct SHeap {
+        TTree** arr;
+        int cap;
+        int cnt;
+    } THeap;
+
+    typedef struct SVector {
+        int* arr;
+        int cap;
+        int cnt;
+    } TVector;
+
+    typedef struct SBitArray {
+        word* arr;
+        int cap;
+        int cnt;
+    } TBitArray;
+
+#endif // !definesANDstructs123
